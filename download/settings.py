39316567a5fd132a3a6b0fb440c2e0985e45b5e4
ekhoshnood <e.khoshnood966@gmail.com>
2020-01-13 23:13:29 +0330

    static problem resolved

--- download/settings.py
+++ download/settings.py
@@ -120,25 +120,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-'''
-STATICFILES_DIRS =[
-    os.path.join(BASE_DIR, 'static_cdn'),
-    os.path.join(BASE_DIR, 'media_cdn'),
-]
-'''
-# STATIC_URL = '/static/'
+
 MEDIA_URL = '/media/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
 #--------------------------------------------------
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #-----------------------------------------------------
-# STATICFILES_DIRS = [
-#         os.path.join(BASE_DIR, 'static'),
-#     ]
+
 
 
