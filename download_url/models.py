7519ff67a37c055e9fae948714981e64ef7e2558
ekhoshnood <e.khoshnood966@gmail.com>
2020-01-23 21:14:03 +0330

    static problem resolved

--- download_url/models.py
+++ download_url/models.py
@@ -37,10 +37,30 @@ class Channel(models.Model):
     phone                   = models.IntegerField(null=True)
     date_purchased          = models.DateField(auto_now_add=True) # TODO: date field will be added when purchasing with this (datetime.datetime.today().date())
     is_purchased            = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.chat_username
+
+
+class SaleType(models.Model):
     # wholesale or retail
+    saletype                = models.CharField(max_length=50, null=True)
+
     is_wholesale = models.BooleanField(default=False)
     is_retail = models.BooleanField(default=False)
-    # JOBS
+
+    def __str__(self):
+        return self.saletype
+
+
+
+
+
+class Senf(models.Model):
+    #shoes, glassess or ...
+    senf                = models.CharField(max_length=50, null=True)
+
+
     shoes = models.BooleanField(default=False)
     bags = models.BooleanField(default=False)
     women_clothes = models.BooleanField(default=False)
@@ -53,20 +73,16 @@ class Channel(models.Model):
     glasses = models.BooleanField(default=False)
     electrical = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.senf
 
-def __str__(self):
-        return self.chat_username
-
-def upload_location(*args, **kwargs):
-    file_path = 'media/text1.jpg'
-    return file_path
 
 
 class Post(models.Model):
     channel                 = models.ForeignKey(Channel, on_delete=models.CASCADE)
     message_id              = models.IntegerField(blank=True, null=True)
     date                    = models.IntegerField(blank=True, null=True)
-    text                    = models.TextField(max_length=5000)
+    text                    = models.TextField(max_length=5000, null=True)
     image                   = models.ImageField(blank=True, null=True)
 
     def __str__(self):
